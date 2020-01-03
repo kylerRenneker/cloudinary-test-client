2ae75db593d9c3f38f5794b7cae271280902b073
a764579 <kyler.renneker@atos.net>
2020-01-03 17:01:31 -0500

    working image upload

--- src/App.js
+++ src/App.js
@@ -3,26 +3,26 @@ import config from './config'
 
 function App() {
   const [file, setFile] = useState(null)
+  const [img, setImg] = useState(null)
 
   const handleImageSubmit = ev => {
     ev.preventDefault()
-    
-    //const { image } = ev.target
-    console.log(file)
 
     const form = new FormData()
     form.append('image', file)
 
     fetch(`${config.API_ENDPOINT}/upload-image`, {
       method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        // 'Content-Type': 'multipart/form-data'
-      },
       body: form
-    
-    }).then(res => {
-      console.log(res)
+    })
+    .then(res => {
+      return !res.ok ? res.json().then(e => Promise.reject(e)) : res.json()
+    })
+    .then(res => {
+      setImg(res.imageUrl)
+    })
+    .catch(err => {
+      console.log(err)
     })
   }
 
@@ -33,9 +33,10 @@ function App() {
   return (
     <div className="App">
       <form onSubmit={ev => handleImageSubmit(ev)} >
-        <label for="image-upload"></label>
-        <input onChange={onChange} id="image_upload" name="image" type="file" accept="image/x-jpg"></input>
+        <label htmlFor="image-upload"></label>
+        <input onChange={onChange} id="image_upload" name="image" type="file"></input>
         <button type="submit">submit</button>
+        {img ? <img src={img} alt='cloudinary'></img> : null}
       </form>
     </div>
   );
